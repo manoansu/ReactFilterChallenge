eb8242e841381194e5f880e9deeb7f2088f70fea
Ansumane Mané <manoansu@gmail.com>
2024-06-14 16:33:06 +0100

    Implementing manualy the project

--- src/components/ListingFilterCart/index.tsx
+++ src/components/ListingFilterCart/index.tsx
@@ -6,8 +6,38 @@ export default function ListingFilterCart() {
             <section id='listing-section' className='container mt30'>
                 <div className='listing-content'>
                     <div className='listing-item'>
-                    PC Gamer Pro
-                    R$ 1200.00
+                    <h4>PC Gamer Pro</h4>
+                    <h6>R$ 1200.00</h6>
+                    </div>
+                </div>
+                <div className='listing-content'>
+                    <div className='listing-item'>
+                    <h4>PC Gamer Pro</h4>
+                    <h6>R$ 1200.00</h6>
+                    </div>
+                </div>
+                <div className='listing-content'>
+                    <div className='listing-item'>
+                    <h4>PC Gamer Pro</h4>
+                    <h6>R$ 1200.00</h6>
+                    </div>
+                </div>
+                <div className='listing-content'>
+                    <div className='listing-item'>
+                    <h4>PC Gamer Pro</h4>
+                    <h6>R$ 1200.00</h6>
+                    </div>
+                </div>
+                <div className='listing-content'>
+                    <div className='listing-item'>
+                    <h4>PC Gamer Pro</h4>
+                    <h6>R$ 1200.00</h6>
+                    </div>
+                </div>
+                <div className='listing-content'>
+                    <div className='listing-item'>
+                    <h4>PC Gamer Pro</h4>
+                    <h6>R$ 1200.00</h6>
                     </div>
                 </div>
             </section>
